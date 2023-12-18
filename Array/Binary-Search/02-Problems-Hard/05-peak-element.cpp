1553d5560bf5db6e0d4e1fb23db0e5cf1183d413
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2023-12-19 04:50:34 +0530

    Added Peak Element Code Question

---
+++ Array/Binary-Search/02-Problems-Hard/05-peak-element.cpp
@@ -0,0 +1,19 @@
+/*
+ * Find Peak Element
+ * A peak element is an element that is strictly greater than its neighbors.
+ * 
+ * Example 1:
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: Index 2 is peak element
+
+ * Example 1:
+ * Input: nums = [1,2,1,3,5,6,]
+ * Output: 1 or 5
+ * Explanation: Index 1 or 5 are peak elements
+
+ * https://leetcode.com/problems/single-element-in-a-sorted-array/
+ * https://www.codingninjas.com/codestudio/problems/unique-element-in-sorted-array_1112654?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf 
+*/
+
+// TODO
