03ade4bbfbf4e60968dd9d3906a61071b74877ca
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-03-16 09:27:02 +0530

    Deleting in LL

--- Linked-List/Linkedlist-Problems/01-Problems-Easy/00-array-to-ll.cpp
+++ Linked-List/Linkedlist-Problems/01-Problems-Easy/00-array-to-ll.cpp
@@ -21,7 +21,7 @@ Node* arrayToLL(std::vector<int>arr) {
   Node* head = NULL;
   head = new Node(arr[0]);
   Node* mover = head;
-  for(int i=1; i<=arr.size(); i++) {
+  for(int i=1; i<arr.size(); i++) {
     Node* newNode = new Node(arr[i]);
     mover->next = newNode;
     mover = newNode; 
@@ -30,14 +30,20 @@ Node* arrayToLL(std::vector<int>arr) {
   return head;
 }
 
+// * Print the LL
+void printLL(Node* head) {
+  Node* temp = head;
+  while(temp) {
+    std::cout<<temp->data<<std::endl;
+    temp = temp->next; 
+  }
+}
+
 int main() {
   std::vector<int> arr = { 1,2,3,4,5 };
   Node* head = arrayToLL(arr);
   Node* temp = head;
   std::cout<<"Elements In LL"<<std::endl;
-  while(temp->next != NULL) {
-    std::cout<<temp->data<<std::endl;
-    temp = temp->next;
-  }
+  printLL(head);
   return 0;
 }
