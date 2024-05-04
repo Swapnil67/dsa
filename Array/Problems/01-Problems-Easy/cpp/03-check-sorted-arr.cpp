28983015b03865b0af348ed52b998cb22446d08d
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-05-04 10:37:28 +0530

    Two more problems

---
+++ Array/Problems/01-Problems-Easy/cpp/03-check-sorted-arr.cpp
@@ -0,0 +1,44 @@
+/**
+ * *  Largest Element in the Array
+ * * You have been given an array ‘a’ of ‘n’ non-negative integers.
+ * * You have to check whether the given array is sorted in the non-decreasing order or not.
+
+ * * Example 1
+ * * Input  : ‘n’ = 5, ‘a’ = [1, 2, 3, 4, 5]
+ * * Output : 1
+ * 
+ * * Example 2
+ * * Input  : ‘n’ = 5, ‘a’ = [4, 5, 4, 4, 4]
+ * * Output : 0
+
+ * * https://www.naukri.com/code360/problems/ninja-and-the-sorted-check_6581957
+*/
+
+
+#include<iostream>
+
+bool checkSorted(std::vector<int> arr) {
+  bool isSorted = true;
+  int n = arr.size();
+  for(int i=0; i<n; i++) {
+    if(arr[i+1] <= arr[i] && i < n-1) {
+      isSorted = false;
+    }
+  }
+  return isSorted;
+}
+
+int main() {
+  // * testcase 1
+  std::vector<int> arr = { 1,2,3,4,5 };
+  // * testcase 2
+  // std::vector<int> arr = { 4,5,4,4,4 };
+
+  bool isSorted = checkSorted(arr);
+  std::cout<<"Is Array Sorted: "<<isSorted<<std::endl; 
+
+  return 0;
+}
+
+// * Run the code
+// * g++ --std=c++17 03-check-sorted-arr.cpp -o 03-check-sorted-arr && ./03-check-sorted-arr
---
+++ Array/Problems/01-Problems-Easy/cpp/04-remove-duplicates-arr.cpp
@@ -0,0 +1,74 @@
+/**
+ * * Remove Duplicates from Sorted Array
+ * * You are given a sorted integer array 'arr' of size 'n'.
+ * * You need to remove the duplicates from the array such that each element appears only once.
+
+ * * Example 1
+ * * Input  : ‘n’ = 5, ‘a’ = [1 2 2 2 3].
+ * * Output : [1 2 3]
+ * 
+ * * Example 2
+ * * Input  : ‘n’ = 10, ‘a’ = [1 2 2 3 3 3 4 4 5 5]
+ * * Output : [1 2 3 4 5]
+
+ * * https://www.naukri.com/code360/problems/remove-duplicates-from-sorted-array_1102307
+*/
+
+#include<set>
+#include<iostream>
+
+void printArr(std::vector<int> arr) {
+  for(int i=0; i<arr.size(); i++) {
+    std::cout<<arr[i]<<" ";
+  }
+  std::cout<<std::endl;
+}
+
+// * ------------------------- APPROACH 1: BRUTE FORCE APPROACH -------------------------`
+// * TIME COMPLEXITY O(NLogN) + O(N)
+// * SPACE COMPLEXITY O(N)
+int bruteForce(std::vector<int> arr) {
+  std::set<int> st;
+  int n = arr.size();
+  // * O(N)
+  for(int i=0; i<n; i++) {
+    // * logN
+    st.insert(arr[i]);
+  }
+
+  int idx = 0;
+  // * O(N)
+  for (int x: st) {
+    arr[idx++] = x;
+  }
+
+  return idx;
+}
+
+// * ------------------------- APPROACH 2: Optimal FORCE APPROACH -------------------------`
+// * TIME COMPLEXITY O(N)
+// * SPACE COMPLEXITY O(1)
+int optimalApproach(std::vector<int> arr) {
+  int n = arr.size();
+  int i=0;
+  for(int j=1; j<n; j++) {
+    if(arr[j] != arr[i]) {
+      arr[i+1] = arr[j];
+      i++;
+    }
+  }
+  return i+1;
+}
+
+int main() {
+  std::vector<int> arr = {1, 2, 2, 3, 3, 3, 4, 4, 5, 5};
+  std::cout<<"Input Vector "<<std::endl;
+  printArr(arr);
+  // int uniqueElements = bruteForce(arr);
+  int uniqueElements = optimalApproach(arr);
+  std::cout<<"Unique Elements "<<uniqueElements<<std::endl;
+}
+
+
+// * Run the code
+// * g++ --std=c++17 04-remove-duplicates-arr.cpp -o 04-remove-duplicates-arr && ./04-remove-duplicates-arr
