61bb3d4e98f2441bb2170d29b70f04b887b9bfb5
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-05-08 05:53:15 +0530

    Max consecutive ones

---
+++ Array/Problems/01-Problems-Easy/cpp/10-max-consecutive-ones.cpp
@@ -0,0 +1,54 @@
+/**
+ * * Max Consecutive Ones
+ * * Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ * * Example 1
+ * * Input  : 'arr' = [1,1,0,1,1,1]
+ * * Output : 3
+
+ * * Example 2
+ * * Input  : 'arr' = [1,0,1,1,0,1]
+ * * Output : 2
+
+ * * https://leetcode.com/problems/max-consecutive-ones/description/
+ * * https://www.naukri.com/code360/problems/traffic_6682625
+*/
+
+#include<iostream>
+
+void printArr(std::vector<int> arr) {
+  for(int i=0; i<arr.size(); i++) {
+    std::cout<<arr[i]<<" ";
+  }
+  std::cout<<std::endl;
+}
+
+// * ------------------------- APPROACH 1: BRUTE FORCE APPROACH -------------------------`
+// * TIME COMPLEXITY O(N)
+// * SPACE COMPLEXITY O(1)
+int bruteForce(std::vector<int> arr) {
+  int n = arr.size(), c = 0, maxOnes = 0;
+  for(int i=0; i<n; i++) {
+    if(arr[i] == 1) {
+      c++;
+      maxOnes = std::max(c, maxOnes);
+    } else {
+      c = 0;
+    }
+  }
+  return maxOnes;
+}
+
+
+int main() {
+  std::vector<int> arr = {1, 1, 0, 1, 1, 1};
+  // std::vector<int> arr = {1, 0, 1, 1, 0, 1};
+  int maxOnes = bruteForce(arr);
+  // int missingNumber = betterApproach(arr);
+  // int missingNumber = optimal(arr);
+  std::cout << "Max Consecutive Ones: " << maxOnes << std::endl;
+  return 0;
+}
+
+// * Run the code
+// * g++ --std=c++17 10-max-consecutive-ones.cpp -o 10-max-consecutive-ones && ./10-max-consecutive-ones
