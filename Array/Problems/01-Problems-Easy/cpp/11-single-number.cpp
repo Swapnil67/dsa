b2af7430f5fd9c20722906b3b41b7426ded91857
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-05-09 23:13:58 +0530

    Single Number in arr

--- Array/Problems/01-Problems-Easy/cpp/11-single-number.cpp
+++ Array/Problems/01-Problems-Easy/cpp/11-single-number.cpp
@@ -18,6 +18,7 @@
  * * https://www.naukri.com/code360/problems/find-the-single-element_6680465
 */
 
+#include<map>
 #include<iostream>
 
 // * ------------------------- APPROACH 1: BRUTE FORCE APPROACH -------------------------`
@@ -41,15 +42,82 @@ int bruteForce(std::vector<int> arr) {
 }
 
 // * ------------------------- APPROACH 2: Better APPROACH -------------------------`
+// * Map Data Structure
+// * TIME COMPLEXITY O(NlogM) + O(N/2)+1
+// * M = size of the map
+// * SPACE COMPLEXITY O(N/2)+1
 int betterApproach(std::vector<int> arr) {
+  std::map<int, int> numMap;
+  int n = arr.size();
+  // * O(NlogM)
+  for(int i=0; i<n; i++) {
+    numMap[arr[i]]++;
+  }
+
+  // * O(N/2) + 1
+  for(auto it: numMap) {
+    if(it.second == 1) {
+      return it.first;
+    }
+  }
+
+  return 0;
+}
+
+// * ------------------------- APPROACH 3: Optimal A APPROACH -------------------------`
+// * XOR Method [For UnSorted Array]
+// * TIME COMPLEXITY O(N)
+// * SPACE COMPLEXITY O(1)
+int optimalA(std::vector<int> arr) {
+  int xr = 0;
+  for(int i=0; i<arr.size(); i++) {
+    xr = xr ^ arr[i];
+    // std::cout<<"xr "<<xr<<std::endl;
+  }
+  return xr;
+}
+
+// * ------------------------- APPROACH 4: Optimal B APPROACH -------------------------`
+// * Binary Search [For Sorted Array]
+// * TIME COMPLEXITY O(logN)
+// * SPACE COMPLEXITY O(1)
+int optimalB(std::vector<int> arr) {
+  int l = 0, r = arr.size()-1;
+
+  // * Boundary Cases
+  if(r == 0)
+    return arr[0];
+  else if(arr[0] != arr[1]) 
+    return arr[0];
+  else if(arr[r] != arr[r-1])
+    return arr[r];
   
+  while(l <= r) {
+    int mid = l+(r-l)/2;
+    // * Unique Element
+    if(arr[mid] != arr[mid+1] && arr[mid] != arr[mid-1]) {
+      return arr[mid];
+    }
+
+    if(((mid%2 == 0) && arr[mid] == arr[mid+1]) || ((mid%2 == 1) && arr[mid] == arr[mid-1])) { 
+      l = mid+1;
+    }
+    else {
+      r = mid-1;
+    }
+  }
+  return 0;
 }
 
 
 int main() {
   // std::vector<int> arr = { 1, 1, 2, 3, 3, 4, 4 };
-  std::vector<int> arr = { 8, 8, 9, 9, 10 };
-  int singleNumber = bruteForce(arr);
+  // std::vector<int> arr = { 8, 8, 9, 9, 10 };
+  std::vector<int> arr = { 1, 1, 2, 2, 3, 3, 6, 6, 7, 9, 9 };
+  // int singleNumber = bruteForce(arr);
+  // int singleNumber = betterApproach(arr);
+  // int singleNumber = optimalA(arr);
+  int singleNumber = optimalB(arr);
   std::cout<<"Single Number "<<singleNumber<<std::endl;
   return 0;
 }
