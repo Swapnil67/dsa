53d056dd4a7ab5153a2711f805c2c23b83bbc585
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-05-16 05:04:09 +0530

    3 Sum

---
+++ Array/Problems/03-Problems-Expert/cpp/03-three-sum.cpp
@@ -0,0 +1,166 @@
+/**
+ * * Three Sum
+ * * You are given an array ‘ARR’ containing ‘N’ integers.
+ * * Return all the unique triplets [ARR[i], ARR[j], ARR[k]] such that i != j, j != k and k != i and their 
+ * * sum is equal to zero i.e. nums[i] + nums[j] + nums[k] == 0.
+
+ * * Example 1
+ * * Input  : 'nums' = [-1,0,1,2,-1,-4]
+ * * Output : [[-1,-1,2],[-1,0,1]]
+ * 
+ * * Example 2
+ * * Input  : 'nums' = [0,1,1]
+ * * Output : []
+
+ * * https://www.naukri.com/code360/problems/three-sum_6922132
+ * * https://leetcode.com/problems/3sum/description/
+*/
+
+#include<set>
+#include<iostream>
+
+// * ------------------------- UTILITY FUNCTIONS -------------------------`
+
+void printArr(std::vector<int> arr) {
+  int n = arr.size();
+  for(int i=0; i<n; i++) {
+    std::cout<<arr[i]<<" ";
+  }
+  std::cout<<std::endl;
+}
+
+// * Time Complexity O(N^2)
+void printMatrix(std::vector<std::vector<int>>&mat) {
+  int n = mat.size();
+  int m = mat[0].size();
+  for(int i=0; i<n; i++) {
+    for(int j=0; j<m; j++) {
+      std::cout<<mat[i][j]<<" ";
+    }
+    std::cout<<std::endl;
+  }
+}
+
+// * ------------------------- APPROACH 1: BRUTE FORCE APPROACH -------------------------`
+// * TIME COMPLEXITY O(N^3) x log(no of tripletes) = O(N^3) x log(m)
+/*
+ * O(N^3) => Three for loop
+ * log(no of tripletes) => Set insert
+*/
+// * SPACE COMPLEXITY 2 x O(no of tripletes)
+std::vector<std::vector<int>> bruteForce(std::vector<int> arr) {
+  int n = arr.size();
+  std::set<std::vector<int>> vectorSet;
+  for(int i=0; i<n; i++) {
+    for(int j=i+1; j<n; j++) {
+      for(int k=j+1; k<n; k++) {
+        int sum = arr[i] + arr[j] + arr[k];
+        // std::cout<<arr[i]<<" + "<<arr[j]<<" + "<<arr[k]<<" = "<<sum<<std::endl;
+        if(sum == 0) {
+          std::vector<int> temp = { arr[i], arr[j], arr[k] };
+          std::sort(temp.begin(), temp.end());
+          vectorSet.insert(temp);
+        }
+      }
+    }
+  }
+
+  std::vector<std::vector<int>> ans(vectorSet.begin(), vectorSet.end());
+  return ans;
+}
+
+
+// * ------------------------- APPROACH 2: BETTER APPROACH -------------------------
+// * ****** Intuition ******
+// * If the following is true
+//* arr[i] + arr[j] + arr[k] = 0 
+// * Then we can also derive the following
+//* arr[k] = -(arr[i] + arr[j]);
+
+// * TIME COMPLEXITY O(N^2) x log(size of set) = O(N^2) x log(m)
+/*
+ * O(N^2) => Two for loop
+ * log(m) => hashset (st)
+*/
+// * SPACE COMPLEXITY O(N) x O(no of triplets) x 2
+/*
+ * O(N) => hashset   => O(N) (Worst Case)
+ * O(no of triplets) => tripletSet
+ * O(no of triplets) => ans
+*/
+std::vector<std::vector<int>> betterApproach(std::vector<int> arr) {
+  int n = arr.size();
+  std::set<std::vector<int>> tripletSet;
+  for(int i=0; i<n; i++) {
+    std::set<int> st;
+    for(int j=i+1; j<n; j++) {
+      int k = -(arr[i] + arr[j]);
+      if(st.find(k) != st.end()) {
+        // * found a triplet
+        std::vector<int> temp = { arr[i], arr[j], k };
+        sort(temp.begin(), temp.end());
+        tripletSet.insert(temp);
+      }
+      st.insert(arr[j]);
+    }
+  }
+  std::vector<std::vector<int>> ans(tripletSet.begin(), tripletSet.end());
+  return ans;
+}
+
+// * ------------------------- APPROACH 3: OPTIMAL APPROACH (TWO POINTERS) -------------------------
+// * TIME COMPLEXITY O(nlogn) + O(N^2)
+/*
+ * O(nlogn) => Sort the initial array
+ * O(N^2) => Nested loop
+*/
+// * SPACE COMPLEXITY O(no of triplets)
+// * O(no of triplets) => ans
+std::vector<std::vector<int>> optimalApproach(std::vector<int> arr) {
+  sort(arr.begin(), arr.end()); // * n(logn)
+  int n = arr.size(); 
+  std::vector<std::vector<int>> ans;
+
+  // * O(N^2)
+  for(int i=0; i<n; i++) {
+    if(i > 0 && arr[i-1] == arr[i]) continue;
+    int j = i+1;
+    int k = n-1;
+    while(j < k) {
+      int sum = arr[i] + arr[j] + arr[k];
+      if(sum == 0) {
+        ans.push_back({ arr[i], arr[j], arr[k] });
+        j++;
+        k--;
+        while(arr[j] == arr[j-1]) {
+          j++;
+        }
+        while(arr[k] == arr[k+1]) {
+          k--;
+        }
+      }
+      else if(sum < 0) {
+        j++;
+      }
+      else {
+        k--;
+      }
+    }
+  }
+  return ans;
+}
+
+int main() {
+  std::vector<int> arr = {-1, 0, 1, 2, -1, -4};
+  std::cout<<"Input Array"<<std::endl;
+  printArr(arr);
+  // std::vector<std::vector<int>> ans = bruteForce(arr);
+  // std::vector<std::vector<int>> ans = betterApproach(arr);
+  std::vector<std::vector<int>> ans = optimalApproach(arr);
+  std::cout<<"Tripletes which gives 0 sum"<<std::endl;
+  printMatrix(ans);
+  return 0;
+}
+
+// * Run the code
+// * g++ --std=c++17 03-three-sum.cpp -o 03-three-sum && ./03-three-sum
