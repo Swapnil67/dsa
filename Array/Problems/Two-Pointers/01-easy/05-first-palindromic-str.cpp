6c9e2b64a2f7970f6b28f27322d6d6aa5d389158
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-09-24 23:29:58 +0530

    Find First Palindromic String in the Array

--- Array/Problems/Two-Pointers/01-easy/05-first-palindromic-str.cpp
+++ Array/Problems/Two-Pointers/01-easy/05-first-palindromic-str.cpp
@@ -1,15 +1,16 @@
-/**
- * * Find First Palindromic String in the Array
- * * Given an array of strings words, return the first palindromic string in the array. 
- * * If there is no such string, return an empty string "".
-
- * * Example 1
- * * Input  : words = ["abc","car","ada","racecar","cool"]
- * * Output : "ada"
+/*
  * 
- * * Example 2
- * * Input  : words = ["notapalindrome","racecar"]
- * * Output : "racecar"
+ * Find First Palindromic String in the Array
+ * Given an array of strings words, return the first palindromic string in the array. 
+ * If there is no such string, return an empty string "".
+
+ * Example 1
+ * Input  : words = ["abc","car","ada","racecar","cool"]
+ * Output : "ada"
+ * 
+ * Example 2
+ * Input  : words = ["notapalindrome","racecar"]
+ * Output : "racecar"
 
  * * https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/
 */
