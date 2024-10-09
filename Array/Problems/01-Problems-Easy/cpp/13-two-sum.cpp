4865101a9a3d87abebd4dc928e4dbdb51fc4459c
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-10-09 09:27:56 +0530

    Two sum practice

--- Array/Problems/01-Problems-Easy/cpp/13-two-sum.cpp
+++ Array/Problems/01-Problems-Easy/cpp/13-two-sum.cpp
@@ -30,7 +30,7 @@ std::vector<int> bruteForce(std::vector<int>arr, int target) {
   for (int i = 0; i < n; i++) {
     for (int j = i + 1; j < n; j++) {
       if (arr[i] + arr[j] == target) {
-        return { i, j }
+        return { i, j };
       }
     }
   }
