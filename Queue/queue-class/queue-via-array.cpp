8e00bb0a48dd62ddad2fb09e8000919db079c273
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2025-01-09 20:47:21 +0530

    Queue via array

--- Queue/queue-class/queue-via-array.cpp
+++ Queue/queue-class/queue-via-array.cpp
@@ -5,34 +5,57 @@
 #include<vector>
 
 class Queue {
+  private: 
+    int max_size;
   public: 
     int front, rear, cur_size;
     std::vector<int> arr;
     Queue() {
-      this->front = 0;
-      this->rear = 0;
-      this->cur_size = 0;
-      this->arr.resize(100001);
+      this->front = -1;
+      this->rear = -1;
+      this->cur_size = 0; 
+      this->max_size = 100001;
+      this->arr.resize(this->max_size);
     }
 
     // * Enqueue (add) element 'e' at the end of the queue.
     void enqueue(int e) {
-      if(this->rear == 100001) {
+      if(this->cur_size == this->max_size) {
         printf("Queue if full\n");
         return;
       } 
+      else if(this->cur_size == 0) {
+        // * first push
+        this->front = 0;
+        this->rear = 0;
+      } else {
+        // * if rear reaches this->max_size
+        // * then this->rear % this->max_size = 0
+        // * i.e. start from zero again
+        this->rear = (this->rear + 1) % this->max_size;
+      }
       this->arr[rear] = e;
-      this->rear++;
-      this->cur_size++;
+      this->cur_size += 1;
     }
 
     // * Dequeue (retrieve) the element from the front of the queue.
     int dequeue() {
-      if(this->front == this->rear) 
+      if(this->cur_size == 0) {
+        printf("\nQueue is empty\n");
         return -1;
+      }
+      // std::cout << this->front << " " << this->rear << std::endl;
       int x = this->arr[this->front];
-      this->front++;
-      this->cur_size--;
+      if (this->cur_size == 1) {
+        // * Only one element in queue
+        // * Destorying the queue
+        this->front = -1;
+        this->rear = -1;
+      }
+      else {
+        this->front = (this->front + 1) % this->max_size;
+      }
+      this->cur_size -= 1;
       return x;
     }
 
