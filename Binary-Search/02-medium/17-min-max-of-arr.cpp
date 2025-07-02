dd05d0178fcc354b912d84003f8294e50c4943b9
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2025-07-02 20:01:41 +0530

    Minimize Maximum of Array

---
+++ Binary-Search/02-medium/17-min-max-of-arr.cpp
@@ -0,0 +1,125 @@
+/**
+ * * Leetcode - 2439
+ * * Minimize Maximum of Array
+ * * You are given a 0-indexed array nums comprising of n non-negative integers.
+ * * In one operation, you must:
+ * 
+ * * Choose an integer i such that 1 <= i < n and nums[i] > 0.
+ * * - Decrease nums[i] by 1.
+ * * - Increase nums[i - 1] by 1.
+ * *  Return the minimum possible value of the maximum integer of nums after performing any number of operations.
+ * 
+ * * Example 1:
+ * * Input       : nums = [3,7,1,6]
+ * * Output      : 5
+ * * Explanation : 
+ * * One set of optimal operations is as follows:
+ * * - Choose i = 1, and nums becomes [4,6,1,6].
+ * * - Choose i = 3, and nums becomes [4,6,2,5].
+ * * - Choose i = 1, and nums becomes [5,5,2,5].
+ 
+ * 
+ * * https://leetcode.com/problems/minimize-maximum-of-array/description/
+ */
+
+#include <vector>
+#include <math.h>
+#include <iostream>
+#include <algorithm>
+
+void printArr(std::vector<int> arr) {
+  int n = arr.size();
+  for (int i = 0; i < n; i++) {
+    std::cout << arr[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+bool isValid(std::vector<int> arr, int maxVal) {
+  int n = arr.size();
+  for (int i = 0; i < n - 1; ++i) {
+    if (arr[i] > maxVal)
+      return false;
+    long long buffer = maxVal - arr[i];
+    arr[i + 1] = arr[i + 1] - buffer;
+  }
+
+  return arr[n - 1] <= maxVal;
+}
+
+// * ------------------------- APPROACH 1: Optimal APPROACH -------------------------
+// * TIME COMPLEXITY O(nlogn)
+// * SPACE COMPLEXITY O(1)
+int minimizeArrayValue(std::vector<int> arr) {
+  int n = arr.size();
+  int l = 0, r = *std::max_element(arr.begin(), arr.end());
+  int ans = 0;
+  while(l <= r) {
+    int m = l + (r - l) / 2;
+    if (isValid(arr, m)) {
+      ans = m;
+      r = m - 1;
+    }
+    else {
+      l = m + 1;
+    }
+  }
+
+  return ans;
+}
+
+// * ------------------------- APPROACH 2: Optimal APPROACH -------------------------
+// * TIME COMPLEXITY O(N)
+// * SPACE COMPLEXITY O(1)
+int minimizeArrayValue2(std::vector<int> arr) {
+  int n = arr.size();
+  long long sum = 0;
+  int res = -1;
+  for (int i = 0; i < n; ++i) {
+    sum += arr[i];
+    float arr_len = (i + 1) * 1.0;
+    int cur = std::ceil(sum / arr_len);
+    res = std::max(res, cur);
+    std::cout << sum << " " << cur  << std::endl;
+  }
+  return res;
+}
+
+
+int main(void) {
+  // * testcase 1
+  // std::vector<int> arr = {3, 7, 1, 6};
+  // * testcase 2
+  // std::vector<int> arr = {10, 1};
+  // * testcase 3
+  std::vector<int> arr = {13, 13, 20, 0, 8, 9, 9};
+  printArr(arr);
+
+  // int ans = minimizeArrayValue(arr);
+  int ans = minimizeArrayValue2(arr);
+  std::cout << "Minimize Maximum of Array " << ans << std::endl;
+
+  return 0;
+}
+
+// * Run the code
+// * g++ --std=c++20 17-min-max-of-arr.cpp -o output && ./output
+
+// * DRY RUN
+
+// * We are doing following two operations (simultaneouslt) on every pass
+// * arr[i] = arr[i] - 1              - (i > 1 && i < n)
+// * arr[i - 1] = arr[i - 1] + 1          
+
+// * i = 1
+// * 3 7 1 6
+// * 4 6
+
+// * i = 1
+// * 4 6 1 6
+// * 5 5
+
+// * i = 3
+// * 5 5 1 6
+// *     2 5          arr[3] = arr[3] - 1
+
