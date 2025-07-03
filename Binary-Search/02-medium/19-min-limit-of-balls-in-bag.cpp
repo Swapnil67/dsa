ac9511282916373dbabd393c028d55bd6957ff31
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2025-07-03 23:19:26 +0530

    Minimum Limit of Balls in a Bag

---
+++ Binary-Search/02-medium/19-min-limit-of-balls-in-bag.cpp
@@ -0,0 +1,121 @@
+/**
+ * * Leetcode - 1760
+ * * Minimum Limit of Balls in a Bag
+
+ * * You are given an integer array nums where the ith bag contains nums[i] balls. 
+ * * You are also given an integer maxOperations.
+ * 
+ * * You can perform the following operation "at most maxOperations" times:
+ * * Take any bag of balls and divide it into two new bags with a positive number of balls. 
+ * * For example, a bag of 5 balls can become two new bags of 1 and 4 balls, or two new bags of 2 and 3 balls.
+ * 
+ * * Your penalty is the maximum number of balls in a bag. You want to minimize your penalty after the operations.
+ * * Return the minimum possible penalty after performing the operations.
+ * 
+ * * Example 1:
+ * * Input       : nums = [9], maxOperations = 2
+ * * Output      : 3
+ * * Explanation : 
+ * * - Divide the bag with 9 balls into two bags of sizes 6 and 3. [9] -> [6,3].
+ * * - Divide the bag with 6 balls into two bags of sizes 3 and 3. [6,3] -> [3,3,3].
+ * * The bag with the most number of balls has 3 balls, so your penalty is 3 and you should return 3.
+ * 
+ * * Example 2:
+ * * Input       : nums = [2,4,8,2], maxOperations = 4
+ * * Output      : 2
+ * * Explanation : 
+ * * - Divide the bag with 8 balls into two bags of sizes 4 and 4. [2,4,8,2] -> [2,4,4,4,2].
+ * * - Divide the bag with 4 balls into two bags of sizes 2 and 2. [2,4,4,4,2] -> [2,2,2,4,4,2].
+ * * - Divide the bag with 4 balls into two bags of sizes 2 and 2. [2,2,2,4,4,2] -> [2,2,2,2,2,4,2].
+ * * - Divide the bag with 4 balls into two bags of sizes 2 and 2. [2,2,2,2,2,4,2] -> [2,2,2,2,2,2,2,2].
+ * 
+ * * The bag with the most number of balls has 2 balls, so your penalty is 2, and you should return 2.
+ * 
+ * * https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/description/
+ */
+
+#include <vector>
+#include <climits>
+#include <iostream>
+#include <algorithm>
+
+void printArr(std::vector<int> arr) {
+  int n = arr.size();
+  for (int i = 0; i < n; i++) {
+    std::cout << arr[i] << " ";
+  }
+  std::cout << std::endl;
+}
+
+bool isValidPenalty(std::vector<int> nums, int operations, int currentPenalty) {
+  int n = nums.size();
+  int ans = INT_MIN;
+  long long totalOps = 0;
+  for (int &num : nums) {
+    int ops = num / currentPenalty;
+    if (num % currentPenalty == 0)
+      ops -= 1;
+    totalOps += ops;
+  }
+  std::cout << currentPenalty << " => " << totalOps << std::endl;
+  return totalOps <= operations;
+}
+
+// * ------------------------- APPROACH 1: Optimal APPROACH -------------------------
+// * TIME COMPLEXITY O(maxPenalty * logn)
+// * SPACE COMPLEXITY O(1)
+int burteForce(std::vector<int> &nums, int operations) {
+  int n = nums.size();
+  int maxPenalty = *std::max_element(nums.begin(), nums.end());
+  int ans = 0;
+  for (int i = 1; i < maxPenalty; ++i) {
+    int currentPenalty = i;
+    if (isValidPenalty(nums, operations, currentPenalty)) {
+      ans = currentPenalty;
+      break;
+    }
+  }
+  return ans;
+}
+
+// * ------------------------- APPROACH 1: Optimal APPROACH -------------------------
+// * TIME COMPLEXITY O(log(maxPenalty) * log(n))
+// * SPACE COMPLEXITY O(1)
+int minimumSize(std::vector<int> &nums, int operations) {
+  int n = nums.size();
+  int maxPenalty = *std::max_element(nums.begin(), nums.end());
+  int ans = 0;
+  int l = 0, r = maxPenalty;
+  while (l <= r) {
+    int m = l + (r - l) / 2;
+    if (isValidPenalty(nums, operations, m)) {
+      ans = m;
+      r = m - 1;
+    } else {
+      l = m + 1;
+    }
+  }
+  return ans;
+}
+
+int main() {
+  int maxOperations = 2;
+  std::vector<int> nums = {9};
+  
+  // int maxOperations = 4;
+  // std::vector<int> nums = {2, 4, 8, 2};
+
+  std::cout << "Balls" << std::endl;
+  printArr(nums);
+  std::cout << "Operations " << maxOperations << std::endl;
+
+  // int ans = burteForce(nums, maxOperations);
+  int ans = minimumSize(nums, maxOperations);
+  std::cout << "Min balls in bag: " << ans << std::endl;
+
+  return 0;
+}
+
+// * Run the code
+// * g++ --std=c++20 19-min-limit-of-balls-in-bag.cpp -o output && ./output
+
