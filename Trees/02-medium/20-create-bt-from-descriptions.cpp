5cdcb57794cef1ec4857b881d6ebe2ef038980b3
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2025-08-14 12:55:05 +0530

    Create Binary Tree From Descriptions

---
+++ Trees/02-medium/20-create-bt-from-descriptions.cpp
@@ -0,0 +1,147 @@
+/*
+* Leetcode - 2196
+* Create Binary Tree From Descriptions
+
+* You are given a 2D integer array descriptions where descriptions[i] = [parenti, childi, isLefti]
+* indicates that parenti is the parent of childi in a binary tree of unique values. Furthermore,
+
+* - If isLefti == 1, then childi is the left child of parenti.
+* - If isLefti == 0, then childi is the right child of parenti.
+
+* Construct the binary tree described by descriptions and return its root.
+! The test cases will be generated such that the binary tree is valid.
+
+* Example 1:
+*                  50
+*               /     \  
+*             20      80
+*           /   \     /   
+*          15   17   19      
+
+* Input: descriptions = [[20,15,1],[20,17,0],[50,20,1],[50,80,0],[80,19,1]]
+* Output: [50,20,80,15,17,19]
+
+* https://leetcode.com/problems/create-binary-tree-from-descriptions/description/
+*/
+
+#include <map>
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+
+typedef struct TreeNode TreeNode;
+
+struct TreeNode {
+public:
+  int data;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int val) {
+    data = val;
+    left = nullptr;
+    right = nullptr;
+  }
+};
+
+void printArr(std::vector<int> arr) {
+  std::cout << "[ ";
+  for (int i = 0; i < arr.size(); ++i) {
+    std::cout << arr[i] << ", ";
+  }
+  std::cout << "]" << std::endl;
+}
+
+
+std::vector<std::vector<int>> bfsTraversal(TreeNode* root) {
+  std::vector<std::vector<int>> ans;
+  if (!root)
+    return ans;
+
+  std::queue<TreeNode*> q;
+  q.push(root);
+
+  while(!q.empty()) {
+    int n = q.size();
+
+    int is_odd_level = 0;
+    std::vector<int> temp;
+    while(n--) {
+      TreeNode* front = q.front();
+      q.pop();
+
+      temp.push_back(front->data);
+      if (front->left)
+        q.push(front->left);
+      if (front->right)
+        q.push(front->right);
+    }
+
+    ans.push_back(temp);
+  }
+
+  return ans;
+}
+
+// * ------------------------- APPROACH: Optimal Approach -------------------------`
+// * TIME COMPLEXITY O(n)
+// * SPACE COMPLEXITY O(n)
+TreeNode *createBinaryTree(std::vector<std::vector<int>> &descriptions) {
+  std::map<int, TreeNode *> hash;
+  std::unordered_set<int> children; // * They can never be root of the tree
+
+  int n = descriptions.size();
+  for(std::vector<int> &vec: descriptions) {
+    int parent_val = vec[0];
+    int child_val = vec[1];
+    bool is_left = vec[2];
+
+    // * Check if parent already exists
+    if (hash.find(parent_val) == hash.end()) {
+      hash[parent_val] = new TreeNode(parent_val);
+    }
+
+    // * Check if child already exists
+    if (hash.find(child_val) == hash.end()) {
+      hash[child_val] = new TreeNode(child_val);
+    }
+
+    if (is_left == 1) {
+      hash[parent_val]->left = hash[child_val];
+    } else {
+      hash[parent_val]->right = hash[child_val];
+    }
+
+    children.insert(child_val);
+  }
+
+  // * Find the root
+  for(std::vector<int> &vec: descriptions) {
+    const int parent = vec[0];
+    if (!children.count(parent)) {
+      return hash[parent];
+    }
+  }
+
+  return nullptr;
+}
+
+int main(void) {
+  std::cout << "----- Tree Description ------" << std::endl;
+  std::vector<std::vector<int>> descriptions = {{20, 15, 1}, {20, 17, 0}, {50, 20, 1}, {50, 80, 0}, {80, 19, 1}};
+  for (auto &arr : descriptions)
+    printArr(arr);
+
+  std::cout << "\n----- Binary Tree ------" << std::endl;
+  TreeNode* root = createBinaryTree(descriptions);
+  std::vector<std::vector<int>> ans = bfsTraversal(root);
+  for (auto &vec : ans)
+    printArr(vec);
+
+  return 0;
+}
+
+// * Run the code
+// * g++ --std=c++20 20-create-bt-from-descriptions.cpp -o output && ./output
+
