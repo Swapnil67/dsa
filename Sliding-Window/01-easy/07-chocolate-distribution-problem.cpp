3d4e902601619681c18a8932cebdea2d0b5ad2b7
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2025-12-08 23:56:04 +0530

    Chocolate Distribution Problem

--- Sliding-Window/01-easy/07-chocolate-distribution-problem.cpp
+++ Sliding-Window/01-easy/07-chocolate-distribution-problem.cpp
@@ -21,7 +21,7 @@
  * Explanation : The minimum difference between maximum chocolates and minimum chocolates is 4 - 2 = 2 
  *               by choosing following m packets :[3, 2, 4].
 
- * Example 2:
+ * Example 3:
  * Input       : arr = [3, 4, 1, 9, 56], m = 5
  * Output      : 55
  * Explanation : With 5 packets for 5 students, each student will receive one packet, 
