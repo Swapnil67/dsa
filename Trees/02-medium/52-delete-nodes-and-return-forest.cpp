84da3615bf95bc0e101fd8211e2b27904514add2
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2025-12-31 22:53:57 +0530

    Delete Nodes And Return Forest

---
+++ Trees/02-medium/52-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,154 @@
+/*
+* Leetcode - 1110
+* Delete Nodes And Return Forest
+
+* You have n binary tree nodes numbered from 0 to n - 1 where node i has two children leftChild[i] and rightChild[i],
+* return true if and only if all the given nodes form exactly one valid binary tree.
+
+* If node i has no left child then leftChild[i] will equal -1, similarly for the right child.
+
+* Note that the nodes have no values and that we only use the node numbers in this problem.
+
+* Example 1:
+* Input       : root = [1,2,3,4,5,6,7], to_delete = [3,5]
+*
+*                      1
+*                /          \  
+*               2             3
+*            /    \         /   \
+*           4      5       6     7
+*
+
+* Output      : [[1,2,null,4],[6],[7]]
+
+* Example 2:
+* Input       : root = [1,2,4,null,3], to_delete = [3]
+*                  1        
+*                /   \     
+*               2     4
+*                \    
+*                 3
+*
+* Output      : [[1,2,4]]
+* 
+* https://leetcode.com/problems/delete-nodes-and-return-forest/description/
+*/
+
+// ! Google, Amazon
+
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+
+typedef struct TreeNode TreeNode;
+
+struct TreeNode {
+public:
+  int data;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode(int val) {
+    data = val;
+    left = nullptr;
+    right = nullptr;
+  }
+};
+
+template <typename T>
+void printArr(std::vector<T> arr) {
+  std::cout << "[ ";
+  for (int i = 0; i < arr.size(); ++i) {
+    std::cout << arr[i] << ", ";
+  }
+  std::cout << "]" << std::endl;
+}
+
+
+void levelOrderTraversal(TreeNode *root) {
+  if (!root)
+    return;
+
+  std::queue<TreeNode *> q;
+  q.push(root);
+
+  while(!q.empty()) {
+    int n = q.size();
+    // * traverse the whole level
+    while (n--) {
+      TreeNode *node = q.front();
+      q.pop();
+
+      std::cout << node->data << " ";
+
+      if (node->left)
+        q.push(node->left);
+
+      if (node->right)
+        q.push(node->right);
+    }
+    std::cout << std::endl;
+  }
+}
+
+TreeNode *dfs(TreeNode *root, std::unordered_set<int> &del_st, std::vector<TreeNode *> &ans) {
+  if (!root)
+    return nullptr;
+
+  dfs(root->left, del_st, ans);
+  dfs(root->right, del_st, ans);
+
+  if (del_st.count(root->data)) {
+    ans.push_back(root->left);
+    ans.push_back(root->right);
+  }
+
+  return root;
+}
+
+// * ------------------------- APPROACH 1: OPTIMAL APPROACH -------------------------
+// * TIME COMPLEXITY O(n)
+// * SPACE COMPLEXITY O(n)
+std::vector<TreeNode *> delNodes(TreeNode *root, std::vector<int> &to_delete) {
+  std::vector<TreeNode *> ans;
+  if (!root)
+    return ans;
+
+  std::unordered_set<int> del_st(begin(to_delete), end(to_delete));
+  dfs(root, del_st, ans);
+
+  // * Insert root if its not present in delete set
+  if (!del_st.count(root->data)) {
+    ans.push_back(root);
+  }
+
+  return ans;
+}
+
+int main(void) {
+  // * testcase 1
+  TreeNode *root = new TreeNode(1);
+  root->left = new TreeNode(2);
+  root->right = new TreeNode(3);
+  
+  root->left->left = new TreeNode(4);
+  root->left->right = new TreeNode(5);
+  
+  root->right->left = new TreeNode(6);
+  root->right->right = new TreeNode(7);
+  std::vector<int> to_delete = {3, 5};
+  std::cout << "Input BT" << std::endl;
+  levelOrderTraversal(root);
+
+  // * testcase 2
+  std::vector<TreeNode *> ans = delNodes(root, to_delete);
+  for (auto &tree : ans)
+    levelOrderTraversal(tree);
+
+  return 0;
+}
+
+// * Run the code
+// * g++ --std=c++20 52-delete-nodes-and-return-forest.cpp -o output && ./output
+
